3d22bbee88e315470e56b82b0ec8165638df3bab
Manvi Arora <130341603+manvi3007@users.noreply.github.com>
2025-01-03 00:08:07 +0530

    Time: 12 ms (36.85%), Space: 22.8 MB (61.28%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int findMax(vector<int>& piles) {
+        int maxi = INT_MIN;
+        for (int i = 0; i < piles.size(); i++) {
+            maxi = max(maxi, piles[i]); // Fixed shadowing issue
+        }
+        return maxi;
+    }
+
+    double TotalHrs(vector<int>& piles, int hrs) {
+        double ttl = 0;
+        for (int i = 0; i < piles.size(); i++) {
+            ttl += (int)ceil((double)piles[i] / (double)hrs);
+
+
+        }
+        return ttl;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = findMax(piles);
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2; 
+            double tl = TotalHrs(piles, mid);
+            if (tl <= h) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low; 
+    }
+};
