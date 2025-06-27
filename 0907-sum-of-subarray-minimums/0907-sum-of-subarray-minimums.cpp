78e5061160a3667f13f7a432c00bc34911cd06dd
Manvi Arora <130341603+manvi3007@users.noreply.github.com>
2025-06-27 19:30:29 +0530

    Time: 11 ms (88.68%), Space: 47.2 MB (61%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -3,15 +3,17 @@ public:
     vector<int> FindNse(vector<int>& arr) {
         int n = arr.size();
         stack<int> st;
-        vector<int> nse(n, n); 
+        vector<int> nse(n,n);
 
         for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && arr[st.top()] >= arr[i]) {
+            while (!st.empty() && arr[st
+            .top()] >= arr[i]) {
                 st.pop();
             }
             if (!st.empty()) {
                 nse[i] = st.top();
             }
+            
             st.push(i); 
         }
         return nse;
@@ -19,7 +21,7 @@ public:
 
     vector<int> FindPse(vector<int>& arr) {
         int n = arr.size();
-        vector<int> pse(n, -1); 
+        vector<int> pse(n,-1); 
         stack<int> st;
 
         for (int i = 0; i < n; i++) {
@@ -35,16 +37,16 @@ public:
     }
 
     int sumSubarrayMins(vector<int>& arr) {
-        int n = arr.size();
         vector<int> nse = FindNse(arr);
         vector<int> pse = FindPse(arr);
+        int n = arr.size();
         int mod = 1e9 + 7;
         long long total = 0;
 
         for (int i = 0; i < n; i++) {
             long long left = i - pse[i];
             long long right = nse[i] - i;
-            total = (total + left * right % mod * arr[i] % mod) % mod;
+            total = (total + (left * right % mod) * arr[i] % mod) % mod;
         }
         return total;
     }
