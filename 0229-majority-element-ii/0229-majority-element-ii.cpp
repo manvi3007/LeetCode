d332e93d6fc11737dd93c70a1bb3c11434eb7f18
Manvi Arora <130341603+manvi3007@users.noreply.github.com>
2025-09-06 15:10:27 +0530

    Time: 0 ms (100%), Space: 23.5 MB (81.3%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,51 +1,53 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int el1;
+        int el2;
         int cnt1 = 0;
         int cnt2 = 0;
-        int n = nums.size();
-        int el1 = INT_MIN;
-        int el2 = INT_MIN;
-        for (int i =0; i<n;i++){
-            if (cnt1 == 0 && el2 != nums[i]){
-                cnt1 = 1;
+        vector<int > ls;
+        int mini = (int) (nums.size()/3)+1;
+
+
+        for (int i = 0; i<n; i++){
+            if (nums[i] != el2 && cnt1 == 0){
+                cnt1++;
                 el1 = nums[i];
             }
-            else if (cnt2 == 0 && el1 != nums[i]){
-                cnt2 = 1;
+            else if (nums[i] != el1 && cnt2 == 0){
+                cnt2++;
                 el2 = nums[i];
             }
-            else if (el1 == nums[i]){
+            else if (nums[i]==el1){
                 cnt1++;
             }
-            else if (el2  == nums[i]){
+            else if (nums[i] == el2){
                 cnt2++;
             }
-            else {
-                cnt1 --;
-                cnt2 -- ;
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
-        vector<int>ls;
-        int cnt11 = 0; int cnt22 = 0;
-        for (int i = 0;i<n;i++){
-            if(el1 == nums[i]){
-                cnt11++;
+        cnt1 = 0;
+        cnt2 = 0;
+        for (int i = 0; i<n; i++){
+            if (el1 == nums[i]){
+                cnt1++;
             }
-            else if (el2 == nums[i]){
-                cnt22++;
+            if (el2 == nums[i]){
+                cnt2++;
             }
 
+
         }
-        int mini = (int) n/3 +1;
-        if (cnt11 >= mini){
+        if (cnt1 >= mini){
             ls.push_back(el1);
         }
-        if (cnt22 >= mini){
+        if (cnt2 >= mini){
             ls.push_back(el2);
-
         }
-        sort(ls.begin(),ls.end());
         return ls;
     }
 };
