98ae7bfd89eedf5c29d16bf33675599ec2dde653
Manvi Arora <130341603+manvi3007@users.noreply.github.com>
2025-09-11 11:03:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    
+    void merge(vector<int>& arr, int low, int mid , int high){
+        int left = low;
+        int right = mid+1;
+        vector<int>temp;
+
+        while(left <= mid && right<= high){
+            if (arr[left] < arr[right]){
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else{
+                temp.push_back(arr[right]);
+                right++;
+            }
+
+        }
+        while(left <= mid){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        while(right <= high){
+            temp.push_back(arr[right]);
+            right++;
+        }
+        for (int i = low ; i <= high; i++){
+            arr[i]=temp[i-low];
+        }
+  
+    }
+    void ms (vector<int>& arr, int low,int high){
+        if (low == high) return arr;
+        int mid = (low+high)/2;
+       
+
+        ms(arr,low,mid);
+        ms(arr,mid+1,high);
+        merge(arr,low,mid,high);
+        
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        ms(nums,0,n-1);
+        return nums;
+    }
+};
