12f17a500d03add5ad1f0b2a246f3000d95dcf1a
Manvi Arora <130341603+manvi3007@users.noreply.github.com>
2025-09-17 11:44:53 +0530

    Time: 0 ms (100%), Space: 31.3 MB (79.67%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -4,20 +4,22 @@ public:
         int n = nums.size();
         int low = 0;
         int high = n-1;
-        while(low<=high){
-            int mid = (low +high)/2;
+        int ans;
+        while(low <= high){
+            int mid = (low+high)/2;
             if (nums[mid] == target){
                 return mid;
             }
-            else if (nums[mid] < target){
-                low = mid + 1;
+            else if (nums[mid] > target){
+                high = mid-1;
 
             }
             else {
-                high = mid -1;
+                low = mid+1;
             }
             
         }
         return -1;
     }
+
 };
