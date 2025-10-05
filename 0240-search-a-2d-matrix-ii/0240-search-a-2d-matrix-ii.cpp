94e34bb13860f484b72de61f4ad7ac04ada67550
Manvi Arora <130341603+manvi3007@users.noreply.github.com>
2025-10-05 10:47:20 +0530

    Time: 52 ms (62.9%), Space: 18.6 MB (67.99%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row =0;
+        int row = 0;
+        int n = matrix.size();
         int m = matrix[0].size();
         int col = m-1;
-        int n = matrix.size();
-        while(row<n && col>=0){
-            if (matrix[row][col]==target){
-                return true;
-            }
-            else if (matrix[row][col]<target){
+        while(row < n && col >=0){
+            if (matrix[row][col]==target) return true;
+            else if (matrix[row][col] < target){
                 row++;
             }
-            else{
-                col--;
+            else {
+                col --;
             }
+
+
         }
         return false;
-    }
 
+    }
 };
