4db0f21c8d303c5ff3e8a1a974682ba827e5b8c4
Manvi Arora <130341603+manvi3007@users.noreply.github.com>
2025-10-11 09:32:24 +0530

    Time: 0 ms (100%), Space: 13.4 MB (42.67%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,17 +4,20 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
         int low = 0;
-        int high =( n * m )-1;
+        int high = n*m-1;
         while(low <= high){
             int mid = low + (high - low)/2;
-            int row = mid / m;
-            int col = mid % m ;
-            if(matrix[row][col] == target) return true;
-            else if (matrix[row][col] < target) low = mid + 1;
-            else {
-                high = mid -1 ;
+            int row = mid/m;
+            int col = mid % m;
+            if(matrix[row][col]==target){
+                return true;
             }
-
+            else if (matrix[row][col]>target){
+                high = mid -1;
+            }
+            else{
+                low = mid + 1;
+            }
         }
         return false;
     }
