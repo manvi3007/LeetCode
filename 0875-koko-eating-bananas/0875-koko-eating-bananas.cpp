d8fd07b354a3c031c83d456eee4363e1360cef7e
Manvi Arora <130341603+manvi3007@users.noreply.github.com>
2025-10-11 09:42:01 +0530

    Time: 19 ms (11.16%), Space: 23 MB (12.5%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    long long count(vector<int>& piles,int hh ){
-        long long h = 0;
-        for (int i = 0; i < piles.size(); i++) {
-            h += (piles[i] + hh - 1) / hh;
+    long long cal(vector<int>& piles, int h){
+        int n = piles.size();
+        long long sum = 0;
+        for (int i = 0; i <n ; i++){
+            sum += ceil((double)piles[i]/(double)h);
+
         }
-        return h;
+        return sum;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        
         int low = 1;
-        int n = piles.size();
-        int high = *max_element(piles.begin(), piles.end());
+        int high = *max_element(piles.begin(),piles.end());
+        int ans = 0;
         while(low <= high){
             int mid = low + (high - low)/2;
-            long long cc = count(piles,mid);
-            if (cc <= h){
-                high = mid-1;
-            }
-            else{
+            long long cc = cal(piles,mid);
+            if (cc > h){
                 low = mid + 1;
             }
+            else{
+                ans = mid;
+                high = mid -1;
+            }
         }
-        return low;
+        return ans;
     }
 };
