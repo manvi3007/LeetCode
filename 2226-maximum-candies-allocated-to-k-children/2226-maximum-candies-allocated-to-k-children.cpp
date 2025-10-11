e63aa9ceef770decd22af0e9c7798f52bcb963ef
Manvi Arora <130341603+manvi3007@users.noreply.github.com>
2025-10-11 11:13:43 +0530

    Time: 30 ms (39.79%), Space: 88.3 MB (33.33%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    long long count(vector<int>& c, long long p){
+    long long cal (vector<int>& can, int ch){
+        int n = can.size();
         long long sum = 0;
-        for (int i = 0; i < c.size();i++){
-            sum += c[i]/p;
+        for (int i = 0; i < n ; i++){
+            sum += can[i]/ch;
         }
         return sum;
     }
     int maximumCandies(vector<int>& candies, long long k) {
-        int low = 1;
-        long long sum = 0;
-        int ans = 0;
-        for(int i = 0; i < candies.size();i++){
-            sum += candies[i];
-        }
-        if(sum < k){
-            return 0;
-        }
-        long long high = sum/k;
-        while(low<=high){
+        long long low = 1;
+        long long high = *max_element(candies.begin(),candies.end());
+        long long ans = 0;
+        while(low <= high){
             long long mid = low + (high - low)/2;
-            long long pp = count(candies,mid);
-            if(pp >= k){
+            long long cc = cal(candies,mid);
+            if(cc >= k){
                 ans = mid;
                 low = mid +1;
             }
