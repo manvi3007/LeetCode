429aba494bff1446a899cd02e64d21bfa13cef6f
Manvi Arora <130341603+manvi3007@users.noreply.github.com>
2025-11-20 21:06:03 +0530

    Time: 8 ms (80.23%), Space: 11.9 MB (51.9%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -11,13 +11,14 @@ public:
     bool hasCycle(ListNode *head) {
         ListNode* slow = head;
         ListNode* fast = head;
-        while(fast!=NULL && fast -> next != NULL){
-            slow = slow->next;
-            fast = fast->next->next;
-            if (slow == fast){
+
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow -> next;
+            fast = fast -> next -> next;
+
+            if(slow == fast){
                 return true;
             }
-
         }
         return false;
     }
